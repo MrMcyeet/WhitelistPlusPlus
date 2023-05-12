6187b2d1984d6345636f9bb9f85b59c2a96ee0bc
MrMcyeet <68966344+MrMcyeet@users.noreply.github.com>
2023-05-12 10:55:59 -0500

    e
    e

--- src/main/kotlin/me/mcyeet/whitelistplusplus/utils/YamlDocument.kt
+++ src/main/kotlin/me/mcyeet/whitelistplusplus/utils/YamlDocument.kt
@@ -6,60 +6,54 @@ import java.io.FileInputStream
 import java.io.FileNotFoundException
 import java.io.InputStream
 
-class YamlDocument(private val yaml: Map<String, Any>) {
-    companion object {
-        private fun readYaml(yamlFile: File): Map<String, Any> {
-            val yaml = Yaml()
-            val inputStream = FileInputStream(yamlFile)
-            return yaml.load(inputStream) as Map<String, Any>
-        }
-
-        private fun readYaml(yamlInputStream: InputStream): Map<String, Any> {
-            return Yaml().load(yamlInputStream) as Map<String, Any>
+class YamlDocument(private val configFile: File, private val defaultConfigStream: InputStream) {
+    init {
+        if (!configFile.exists()) {
+            configFile.parentFile.mkdirs()
+            configFile.outputStream().use {
+                defaultConfigStream.copyTo(it)
+            }
         }
     }
 
-    constructor(yamlFile: File, defaultYaml: File) : this(
-        if (!yamlFile.exists()) {
-            yamlFile.parentFile.mkdirs()
-            yamlFile.outputStream().use { outputStream ->
-                defaultYaml.inputStream().use { inputStream ->
-                    inputStream.copyTo(outputStream)
-                }
-            }
-            readYaml(yamlFile)
-        } else {
-            readYaml(yamlFile)
+    private val yaml = Yaml()
+    private val configData: MutableMap<String, Any?> = loadConfig()
+
+    private fun loadConfig(): MutableMap<String, Any?> {
+        FileInputStream(configFile).use { fileInputStream ->
+            return yaml.load(fileInputStream) as MutableMap<String, Any?>
         }
-    )
+    }
 
-    constructor(yamlFile: File, defaultYaml: InputStream) : this(
-        if (!yamlFile.exists()) {
-            yamlFile.parentFile.mkdirs()
-            yamlFile.outputStream().use {
-                defaultYaml.copyTo(it)
-            }
-            readYaml(yamlFile)
-        } else {
-            readYaml(yamlFile)
-        }
-    )
-
-    constructor(yamlFile: InputStream) : this(
-        readYaml(yamlFile)
-    )
-
+    @Suppress("UNCHECKED_CAST")
     fun <T> get(path: String): T {
-        val keys = path.split(".")
-        var value: Any? = yaml
-        for (key in keys) {
-            value = (value as Map<String, Any>)[key] ?: throw NoSuchElementException("Key not found: $key")
-        }
-        return value as T
+        if (!this.has(path)) throw NoSuchElementException("Could not find key \"$path\" in ${configFile.path}")
+        return path.split(".").fold(configData) { config: Any?, key ->
+            (config as Map<String, *>)[key]
+        } as T
     }
 
-    fun has(path: String): Boolean {
-        return yaml.contains(path)
+    @Suppress("UNCHECKED_CAST")
+    fun <T> getOrNull(path: String): T? {
+        if (!this.has(path)) return null
+        return path.split(".").fold(configData) { config: Any?, key ->
+            (config as Map<String, *>)[key]
+        } as T
+    }
+
+    fun <T> getOrDefault(path: String, default: T): T {
+        if (!this.has(path)) return default
+        return this.get(path)
+    }
+
+    private fun has(path: String): Boolean {
+        var value: Any? = configData
+        path.split('.').forEach{
+            if ((value !is Map<*, *>) || (!(value as Map<*, *>).containsKey(it)))
+                return false
+            value = (value as Map<*, *>)[it]
+        }
+        return true
     }
 
     inline fun <reified T> isType(path: String): Boolean {
